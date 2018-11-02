31b77283b099024aede20d4b386c9e2f5c5be42b
lishuang <lishuang@jusfoun.com>
2018-11-02 18:07:37 +0800

    feat:导入模型猪

--- src/container/authRoute/authRoute.js
+++ src/container/authRoute/authRoute.js
@@ -37,8 +37,10 @@ class AuthRoute extends Component {
     // renderer.setClearColor(new THREE.Color(0xfff000));
     domEle.appendChild(renderer.domElement);
     let scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x373470);
+    scene.fog = new THREE.Fog(0xffffff, 100, 300);// 雾效果
     let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-    camera.position.set(-29, 36, -50);
+    camera.position.set(0, 15, 100);
     let light = new THREE.AmbientLight({color: 0xffffff});
     light.position.set(1, 1, 1);
     scene.add(light);
@@ -50,56 +52,65 @@ class AuthRoute extends Component {
     renderer.render(scene, camera);
 
     let guiFields = {
-      positionX:0,
-      positionY:0,
-      positionZ:0
+      scaleX: 1,
+      scaleY: 1,
+      scaleZ: 1
     };
     let gui = new dat.GUI();
-    gui.add(guiFields, 'positionX', -1000, 1000).onChange(function (e) {
-      camera.position.x = e;
+    gui.add(guiFields, 'scaleX', 0, 2*Math.PI).onChange(function (e) {
+      OBJ.rotation.x = e
     });
-    gui.add(guiFields, 'positionY', -1000, 1000).onChange(function (e) {
-      camera.position.y = e;
+    gui.add(guiFields, 'scaleY', 0, 2*Math.PI).onChange(function (e) {
+      OBJ.rotation.y = e
     });
-    gui.add(guiFields, 'positionZ', -1000, 1000).onChange(function (e) {
-      camera.position.z = e;
+    gui.add(guiFields, 'scaleZ', 0, 2*Math.PI).onChange(function (e) {
+      OBJ.rotation.z = e
     });
     let loader = new OBJLoader();
     // 导入文件面.obj
-    loader.load('/static/module/face.obj', function (object) {
-      let box = new THREE.Box3();
-      box.expandByObject(object);
-      box.getCenter();
-      object.children[0].geometry.computeBoundingBox();
-      object.children[0].geometry.center();
-      object.traverse(function (child) {
-        if (child instanceof THREE.Mesh) {
-          child.material.transparent = true;
-          child.material.opacity = 0.6;
-          child.material.color.set(0x1f7ccc);
-        }
-      });
-      object.scale.set(0.2, 0.2, 0.2); // 对象缩放
-      scene.add(object);
-    });
+    // loader.load('/static/module/line.obj', function (object) {
+    //   console.log(object);
+    //   let box = new THREE.Box3();
+    //   box.expandByObject(object);
+    //   box.getCenter();
+    //   object.children[0].geometry.computeBoundingBox();
+    //   object.children[0].geometry.center();
+    //   object.traverse(function (child) {
+    //     if (child instanceof THREE.Mesh) {
+    //       child.material.transparent = true;
+    //       child.material.opacity = 0.6;
+    //       child.material.color.set(0x1f7ccc);
+    //     }
+    //   });
+    //   object.scale.set(0.2, 0.2, 0.2); // 对象缩放
+    //   scene.add(object);
+    // });
 
-    // 导入文件线.obj
-    loader.load('/static/module/line.obj', function (object) {
-      let box = new THREE.Box3();
-      box.expandByObject(object);
-      box.getCenter();
-      object.children[0].geometry.computeBoundingBox();
-      object.children[0].geometry.center();
-      object.traverse(function (child) {
-        if (child instanceof THREE.Mesh) {
-          child.material.transparent = true;
-          child.material.opacity = 0.6;
-          child.material.color.set(0x1f7ccc);
-
-        }
-      });
-      object.scale.set(0.2, 0.2, 0.2); // 对象缩放
-      scene.add(object);
+    // 导入猪.obj
+    let geometry, pointGeometry, line;
+    const OBJ = new THREE.Object3D();
+    loader.load('/static/module/pig02.obj', function (object) {
+      console.log(object);
+      geometry = object.children[0].geometry;
+      pointGeometry = new THREE.Points(geometry, new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        size: 1,
+        wireframe: true,
+        transparent: true,
+        opacity: 1
+      }));
+      OBJ.add(pointGeometry);
+      let Wireframe = new THREE.WireframeGeometry(geometry); //线框
+      line = new THREE.LineSegments(Wireframe, new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        lineWidth: 1,
+        transparent: true,
+        opacity: 0.1
+      })); // 线段
+      OBJ.add(line);
+      OBJ.scale.set(0.1, 0.2, 0.1);
+      OBJ.rotation.set(0, 0.6, 0);
+      scene.add(OBJ);
     });
 
     function animate() {
