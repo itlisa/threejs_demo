48116df0896d08b2b3c6208beb21d9f47028ee0e
lishuang <lishuang@jusfoun.com>
2018-11-06 17:42:59 +0800

    fix:优化样式

--- src/component/3dBar/3dbar.js
+++ src/component/3dBar/3dbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as THREE from 'three';
 import OrbitControls from 'three-orbitcontrols';
 import * as d3 from 'd3';
+import * as dat from 'dat.gui';
 import { Timer } from '@jusfoun-vis/common';
 
 /**
@@ -58,6 +59,11 @@ class Bar extends Component {
     return this._obj3D;
   }
 
+  _fontObj = new THREE.Object3D();
+  get fontObj () {
+    return this._fontObj;
+  }
+
   get width () {
     return this._width;
   }
@@ -70,6 +76,15 @@ class Bar extends Component {
     this._width = value;
   }
 
+  _fontSize = 50;
+  get fontSize () {
+    return this._fontSize;
+  }
+
+  set fontSize (value) {
+    this._fontSize = value;
+  }
+
   render () {
     return (
       <div ref='webglbox'></div>
@@ -79,6 +94,8 @@ class Bar extends Component {
   renderer = '';
 
   createBar (data, color, i, obj) {
+    const me = this;
+    const fontSize = me.fontSize;
     const geometry = new THREE.BoxBufferGeometry(20, data, 20);
     const material = new THREE.MeshPhongMaterial({
       color
@@ -86,10 +103,38 @@ class Bar extends Component {
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(20 * i, data / 2, 0);
     mesh.scale.set(0, 0, 0);
-    this.obj3D.add(mesh);
-    // this.obj3D.scale.set(1, 1, 1);
-    this.obj3D.rotation.y = 2.3 * Math.PI;
+    me.obj3D.add(mesh);
+    this.obj3D.scale.set(1, 1, 1);
+    me.obj3D.rotation.y = 2.3 * Math.PI;
+    me.fontObj.rotation.y = 2.3 * Math.PI;
+    // me.fontObj.rotation.z = 0.1 * Math.PI;
     obj.box = mesh;
+    const canvasText = me.createCanvasText(obj.name, '#ffffff');
+    const planeGeometry = new THREE.PlaneBufferGeometry(fontSize, fontSize);
+    const planeMaterial = new THREE.MeshBasicMaterial({
+      map: new THREE.CanvasTexture(canvasText),
+      side: THREE.DoubleSide
+    });
+    const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+    planeMesh.rotation.x = -0.1 * Math.PI;
+    planeMesh.position.set(i * (fontSize - 30) + 10, 0, 40);
+    // planeMesh.scale.set(0.1, 0.1, 0.1);
+    me.fontObj.add(planeMesh);
+  }
+
+  createCanvasText (data, style) {
+    console.log(style);
+    const canvas = document.createElement('canvas');
+    canvas.width = 256;
+    canvas.height = 256;
+    const cxt = canvas.getContext('2d');
+    cxt.beginPath();
+    cxt.fillStyle = style;
+    cxt.font = this.fontSize + 'px Verdana';
+    cxt.fillText(data, 0, this.fontSize);
+    cxt.fill();
+    cxt.stroke();
+    return canvas;
   }
 
   componentDidMount () {
@@ -135,7 +180,9 @@ class Bar extends Component {
       const color = i % 2 ? 0xffffff : 0x006dcb;
       this.createBar(x(datas[i].value), color, i, datas[i]);
     }
+    this.fontObj.position.set(-7, -29, -9);
     scene.add(this.obj3D);
+    scene.add(this.fontObj);
     const timer = new Timer(100, datas.length);
     let index = 0;
     timer.on('timer', function () {
@@ -148,6 +195,23 @@ class Bar extends Component {
     const orbitControls = new OrbitControls(camera, renderer.domElement);
     orbitControls.autoRotate = false;
     renderer.render(scene, camera);
+    //添加调试工具
+    const gui = new dat.GUI();
+    const test = {
+      posx: 0,
+      posy: 0,
+      posz: 0
+    };
+    const me = this;
+    gui.add(test, 'posx', 0, Math.PI * 2).onChange(function (e) {
+      me.fontObj.rotation.x = e;
+    });
+    gui.add(test, 'posy', 0, Math.PI * 2).onChange(function (e) {
+      me.fontObj.rotation.y = e;
+    });
+    gui.add(test, 'posz', 0, Math.PI * 2).onChange(function (e) {
+      me.fontObj.rotation.z = e;
+    });
 
     function animate () {
       orbitControls.update();
